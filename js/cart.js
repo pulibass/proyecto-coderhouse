dcdc56cb444d4bd7f760b3c7eab40f085f7b74ea
Eduardo Ahumada <pulibass@users.noreply.github.com>
2022-03-08 12:22:15 -0300

    cart y usuario

--- js/cart.js
+++ js/cart.js
@@ -1,8 +1,8 @@
 //variables para productos.
 
-/* let imgProducto = document.getElementById('img-producto');
-let titleProducto = document.getElementById('title-producto')
-let priceProducto = document.getElementById('price-producto') */
+let imgProducto = document.getElementById('img-producto');
+let titleProducto = document.getElementById('title-producto');
+let priceProducto = document.getElementById('price-producto');
 
 
 const btnCompra = document.querySelectorAll('#btn-compra'); //con este propiedad selecciono todos los botones de compra
@@ -13,6 +13,92 @@ btnCompra.forEach((añadirAlCarro) => { // uso la variable con un foreach(paso e
 
 function añadirAlCarroClick(event) { //creo a funcion añadirAlCarro que es la que le paso al evento click como parametro
     const boton = event.target; //creo una variable que va a ser el boton que se va a clickear, usando el parametro de la funcion y target se refiere elemento clickeado
-
     console.log(boton)
 }
+
+let producto;
+
+function Producto(nombre, dato, precio, iva, descuento) {
+    this.nombre = nombre.toUpperCase();
+    this.dato = dato.toUpperCase();
+    this.precio = parseInt(precio);
+    this.iva = parseInt(iva);
+    this.descuento = parseInt(descuento);
+    this.calculo = function () {
+
+        let iva = (this.precio * this.iva) / 100;
+        let precioConIva = this.precio + iva;
+        let descuento = (precioConIva * this.descuento) / 100;
+        let precioTotal = precioConIva - descuento;
+        let detalle = document.write("<br>" + "producto seleccionado: " + "<br>" + "<b>" + this.nombre + " " + dato + "<br>" + "</b>" + " Precio: $" + "<b>" + this.precio + "</b>" + "<br>" + "El precio mas el iva del 21% es: $" + "<b>" + precioConIva + "<br>" + "</b>" + "El precio total con el descuento del " + this.descuento + "% es: $" + "<b>" + precioTotal + "</b>" + "<br>");
+        return detalle;
+        /*  let contenedorProducto = document.getElementsByClassName('contenedor-productos');
+         let nuevoProducto = document.createElement('div');
+         nuevoProducto.classList.add('carro-compras');
+         const contenidorAñadido = `
+                     <div class="producto">
+                         <div class="imagen-carro">
+                             <div class="imagen">
+                                 <img src="../img/amortiguadores/amortiguador-4.jpg">
+                             </div>
+                             <div class="title-producto">
+                                 <h2>${nombre}</h2>
+                             </div>
+                         </div>
+                     </div>
+                     <div class="precio">
+                         <div class="price">
+                             <p>${precioTotal}</p>
+                         </div>
+                     </div>
+                     <div class="cantidad">
+                         <div class="button-cantidad">
+                             <div class="btn-number">
+                                 <input type="number" value="number">
+                             </div>
+                             <div class="btn-delete">
+                                 <button><img src="../img/iconos/rectangle-xmark-solid.svg" alt=""></button>
+                             </div>
+                         </div>
+                     </div>`;
+
+         nuevoProducto.innerHTML = contenidorAñadido;
+
+         contenedorProducto.append(nuevoProducto); */
+
+    }
+}
+let productos = []
+
+while (producto != "cubierta" || producto != "amortiguador" || producto != "bateria") {
+
+    producto = prompt("ingrese un producto: cubierta, amortiguador, bateria");
+    switch (producto) {
+        case "cubierta":
+            productos.push(new Producto(producto, " Pirelli 185 65 14", 20000, 21, 30));
+            break;
+        case "amortiguador":
+            productos.push(new Producto(producto, "corven x 4", 15000, 21, 15));
+
+            break;
+        case "bateria":
+            productos.push(new Producto(producto, "moura", 12050, 21, 10));
+
+            break;
+        default:
+            alert("no se ingreso un producto valido");
+            break;
+    }
+
+
+    ingreseNuevamente = prompt("desea ingresar un producto nuevamente? si o no,")
+    if (ingreseNuevamente == "no") {
+        alert("gracias por utilizar nuestros servicios")
+        break;
+    }
+}
+for (const productosInclude of productos) {
+    productosInclude.calculo()
+
+}
+console.table(productos);
--- js/usuario.js
+++ js/usuario.js
@@ -1,24 +1,3 @@
-/* const btnIngresar = document.getElementById("btn-ingresar")
-btnIngresar.addEventListener('click', pageUser)
-let inputUserName = document.getElementById("input-name-user");
-console.log(inputUserName);
-let inputUserPassword = document.getElementById("input-password-user");
-console.log(inputUserPassword)
-
-function pageUser() {
-    let usuario = [];
-    usuario.push(inputUserName)
-    let password = [];
-    password.push(inputUserPassword)
-
-    if (usuario != "admin" && password != "admin") {
-        alert("bienvenido " + inputUserName)
-    } else {
-        alert("usuario incorrecto")
-
-    }
-} */
-
 const btnIngresar = document.getElementById("btn-ingresar")
 
 btnIngresar.addEventListener('click', pageUser)
