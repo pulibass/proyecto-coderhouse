891e3fd0d7c3f0ebe40c1a93df98766f631b7e69
Eduardo Ahumada <pulibass@users.noreply.github.com>
2022-03-25 19:33:43 -0300

    añadiendo localstorage en usuarios

--- js/usuario.js
+++ js/usuario.js
@@ -1,14 +1,17 @@
 const btnIngresar = document.getElementById("btn-ingresar")
 
-btnIngresar.addEventListener('click', pageUser)
+btnIngresar.addEventListener('click', () => {
+    pageUser(inputUserName, inputUserPassword);
+    guardoDatos()
 
-
+});
+let inputUserName;
+let inputUserPassword;
 
 function pageUser() {
-
-    let inputUserName = document.getElementById("input-name-user").value
+    inputUserName = document.getElementById("input-name-user").value
     console.log(inputUserName);
-    let inputUserPassword = document.getElementById("input-password-user").value;
+    inputUserPassword = document.getElementById("input-password-user").value;
     console.log(inputUserPassword)
 
     if (inputUserName != "admin" && inputUserPassword != "admin") {
@@ -17,4 +20,28 @@ function pageUser() {
         swal("no tiene permisos para esta operacion", "error");
 
     }
+    guardoDatos();
 }
+
+const guardoDatos = () => {
+    localStorage.setItem("inputUserName", inputUserName);
+    localStorage.setItem("inputUserPassword", inputUserPassword);
+}
+
+const recuperarDatos = () => {
+    inputUserName = localStorage.getItem("inputUserName");
+    inputUserPassword = localStorage.getItem("inputUserPassword");
+
+}
+
+const limpiarLocalStorage = () => {
+    const resp = confirm("¿Realmente deseas eliminar los datos del LS?");
+    if (resp) {
+        localStorage.clear();
+        console.log("se han eliminado los datos");
+    }
+}
+
+
+recuperarDatos();
+
